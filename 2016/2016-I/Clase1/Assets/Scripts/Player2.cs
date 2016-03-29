8605c147f8c45ccc6a6e4c3d7fd06b04993beb10
Profesor <Profesor>
2016-03-28 22:43:51 -0500

    clase2

--- 2016/2016-I/Clase1/Assets/Scripts/Player2.cs
+++ 2016/2016-I/Clase1/Assets/Scripts/Player2.cs
@@ -6,9 +6,13 @@ public class Player2 : MonoBehaviour {
 	// Use this for initialization
 	public float speed;
 	public int directionX = -1;
+	public int rebote;
+	public float boost;
+	private int tmp_rebotes;
 	private float scaleX;
 	void Start () {
 		scaleX = transform.localScale.x;
+		tmp_rebotes = 0;
 	}
 	
 	// Update is called once per frame
@@ -16,12 +20,24 @@ public class Player2 : MonoBehaviour {
 		Vector3 position = transform.position;
 		if (position.x > 5) {
 			directionX = -1;
+			tmp_rebotes++;
 			scaleX = scaleX * -1;
 		}
 		else if (position.x < -5) {
+			tmp_rebotes++;
 			directionX = 1;
 			scaleX = scaleX * -1;
 		}
+		/*if (tmp_rebotes > 0) {
+			if (tmp_rebotes % rebote == 0) {
+				
+			}
+		}*/
+		if (tmp_rebotes >= rebote) {
+			tmp_rebotes = 0;
+			speed += boost;
+		}
+
 		transform.localScale = new Vector3 (scaleX,
 			transform.localScale.y,
 			transform.localScale.z);
---
+++ 2016/2016-I/Clase2/Assets/Scripts/Spawner.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+	public GameObject candy1;
+	public GameObject candy2;
+	public GameObject candy3;
+	public GameObject candy4;
+	public GameObject candy5;
+
+	public float totalTime;
+	float time;
+
+	public float reductionTime;
+	float rTime;
+
+	void Start () {
+		time = 5f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rTime += Time.deltaTime;
+		if (rTime >= reductionTime) {
+			//totalTime--;
+			if(totalTime>0){
+				totalTime -= 1;	
+			}
+			rTime = 0;
+			//totalTime = totalTime - 1;
+		}
+
+
+		time += Time.deltaTime;
+		if (time >= totalTime) {
+			int candy = Random.Range (1, 5);
+			float posX = Random.Range (-5f, 5f);
+			Vector3 position = new Vector3 (posX, transform.position.y, 0);
+			time = 0;
+			switch(candy){
+				case 1:
+					Instantiate(candy1,
+						position,
+						transform.rotation);
+					break;
+				case 2:
+					Instantiate(candy2,
+						position,
+						transform.rotation);
+					break;
+				case 3:
+					Instantiate(candy3,
+						position,
+						transform.rotation);
+					break;
+				case 4:
+					Instantiate(candy4,
+						position,
+						transform.rotation);
+					break;
+				default:
+					Instantiate(candy5,
+						position,
+						transform.rotation);
+					break;
+			}
+
+		}
+	}
+}
